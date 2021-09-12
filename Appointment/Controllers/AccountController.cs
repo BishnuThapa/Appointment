8631739b3c73fcc51feccca28c075f9d5497a4b4
Bishnu Thapa <thapabishnu20@gmail.com>
2021-09-12 15:26:33 +0545

    added  Account controller & provided access to database in

---
+++ Appointment/Controllers/AccountController.cs
@@ -0,0 +1,23 @@
+﻿using Appointment.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Appointment.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AccountController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
